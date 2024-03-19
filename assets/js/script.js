020128097d447fa57ac6ae5cdc78b112972f25f4
Larry Wisniewski <larryw48390@gmail.com>
2024-03-19 02:06:01 -0400

    working on modal JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 const storedPlaces = JSON.parse(localStorage.getItem('places'));
+const searchDiv = $('#searchDiv');
+const search = $('#searchBar');
+const bodyEl = $('body')
 let savedPlaces = [];
-
 
 function handleSearchClick(input) {
     const geocoder = new google.maps.Geocoder();
@@ -16,10 +18,17 @@ function handleSearchClick(input) {
             const coords = {lat: loc.lat(), lng: loc.lng()};
             console.log(results)
             findRestaurants(coords);
+            search.val('');
         }
         else 
         {
-            alert(`Invalid address: ${input}`);
+            searchDiv.append(`
+            <p id="errorText">${input} is not a valid search!`);
+            search.val('');
+            setTimeout(() => {
+                const text = $('#errorText');
+                text.remove();
+            }, 1000);
         }
     })
 };
@@ -33,13 +42,13 @@ function findRestaurants(coords) {
 
     }
     const service = new google.maps.places.PlacesService(document.createElement('div'));
-    const restList = new Promise((resolve, error) => {
-            service.nearbySearch(query, (results) => {
-                resolve(results)
-                console.log(results);
-                sortRestarants(results);
-            })
+    new Promise((resolve, error) => {
+        service.nearbySearch(query, (results) => {
+        resolve(results);
+        console.log(results);
+        sortRestarants(results);
         })
+    })
 }
 
 function sortRestarants(restaurants) {
@@ -53,13 +62,92 @@ function renderList(restaurants) {
 
 }
 
+function getModalInfo(id) {
+    const request = {
+        placeId: id
+    };
+    const service = new google.maps.places.PlacesService(document.createElement('div'));
+    const restaurant = service.getDetails(request, (place, status) => {
+    if (status === google.maps.places.PlacesServiceStatus.OK) 
+        {
+            console.log(place)
+            let info = {
+                address: place.formatted_address,
+                name: place.name,
+                phone: place.formatted_phone_number,
+                rating: place.rating,
+                reviews: place.reviews,
+                website: place.website
+            };
+            createModal(info);
+        }
+    });
+    console.log(restaurant);
+};
+
+function createModal(restaurant) {
+    bodyEl.append(`
+    <div class="modal">
+        <div class="modal-background"></div>
+        <div class="modal-card">
+            <header class="modal-card-head">
+                <p class="modal-card-title">${restaurant.name}</p>
+                <p>Rating: ${restaurant.rating} ☆</p>
+                <button class="delete" aria-label="close"></button>
+            </header>
+            <section class="modal-card-body">
+                <h2>Reviews</h2>
+                <ul id="reviewUL">
+                </ul>
+            </section>
+            <footer class="modal-card-foot">
+                <button class="button is-success">Save to Favorites</button>
+            </footer>
+        </div>
+    </div>`);
+    addReviews(restaurant.reviews);
+    showModal();
+}
+
+function addReviews(reviews) {
+    const ulEl = $('#reviewUL');
+    for (let i = 0; i < reviews.length; i++)
+    {
+        let author = reviews[i].author_name;
+        let rating = reviews[i].rating;
+        let time = reviews[i].relative_time_description;
+        let text = reviews[i].text;
+        ulEl.append(`
+            <li>
+                <article class="media">
+                    <div class="media-content">
+                        <div class="content">
+                        <p>
+                        <strong>${author}</strong> <small>${rating} ☆</small> <small>${time}</small>
+                        <br>
+                        ${text};
+                        </p>
+                        </div>
+                    </div>
+                </article>
+            </li>
+        `);
+
+    }
+}
+
+function showModal() {
+    const modal = $('.modal');
+    modal.addClass('is-active');
+}
+
+
 //function that runs on document load, adds event listeners to our search button
 $(document).ready(() => {
     const goBtn = $('#goBtn');
-    const searchInput = $('#searchBar');
     goBtn.on('click', () => {
-        console.log(searchInput.val());
-        const input = searchInput.val();
+        console.log(search.val());
+        const input = search.val();
         handleSearchClick(input);
     })
 });
